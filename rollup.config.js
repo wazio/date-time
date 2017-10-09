1d52010f82497ff724c193d2410e8e1f1dcf88b3
Adam Waz <adamwaz3@gmail.com>
2017-10-09 18:52:04 +0200

    Add rollup to package.json and update config properties name.

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,12 @@ import sourceMaps from 'rollup-plugin-sourcemaps';
 const pkg = require('./package.json');
 
 export default {
-  entry: './compiled/index.js',
-  targets: [
-    {dest: pkg.main, moduleName: 'wazio.dateTime', format: 'umd'},
-    {dest: pkg.module, format: 'es'},
+  input: './compiled/index.js',
+  output: [
+    {file: pkg.main, name: 'wazio.dateTime', format: 'umd'},
+    {file: pkg.module, format: 'es'},
   ],
-  sourceMap: true,
+  sourcemap: true,
   plugins: [
     sourceMaps()
   ]
